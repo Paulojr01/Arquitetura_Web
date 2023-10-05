747ebfb3313fd14871ad258934b7fa9a92becc9a
Paulo Alves de Oliveira Junior <12113899@aluno.newtonpaiva.br>
2023-10-04 21:31:00 -0300

    prova

--- ProvaAV1/routes.js
+++ ProvaAV1/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Marker = require('./models/marker'); 
+const Marker = require('./marker'); 
 
 router.get('/location', async (req, res) => {
   try {
