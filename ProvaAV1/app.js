67f086f9c19868eb07789e92199a53e124ddea77
Paulo Alves de Oliveira Junior <12113899@aluno.newtonpaiva.br>
2023-10-04 21:16:45 -0300

    prova

--- ProvaAV1/app.js
+++ ProvaAV1/app.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
+app.listen(3000, function(){
+    console.log('APP rodando na porta 3000');
+  });
